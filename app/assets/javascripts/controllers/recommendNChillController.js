e9bcbbd951767fab06e03b8d865ce0f620ebfdb4
Gabrielle <gabivk@gmail.com>
2016-04-26 14:01:34 +0100

    passing feature test

--- app/assets/javascripts/controllers/recommendNChillController.js
+++ app/assets/javascripts/controllers/recommendNChillController.js
@@ -1,5 +1,9 @@
 recommendNChill.controller('recommendNChill', function(){
+  var self = this
+  self.movies = ['Armageddon', 'Scorpion King'];
 
-  this.movies = ['Armageddon', 'Scorpion King'];
+  self.addMovie = function(newMovie){
+    this.movies.push(newMovie);
+  };
 
 });
--- spec/test/unit/recommendNChillController.spec.js
+++ spec/test/unit/recommendNChillController.spec.js
@@ -11,4 +11,9 @@ describe('recommendNChillController', function(){
     var movies = ['Armageddon', 'Scorpion King'];
     expect(ctrl.movies).toEqual(movies);
   });
+
+  it('adds a movie to the list', function(){
+    ctrl.addMovie('Pitch Perfect');
+    expect(ctrl.movies.pop()).toEqual('Pitch Perfect');
+  });
 });
