f9569528c5c7e67cfe93be14db0bd1dc9e943a3c
Jazzy <jasmina.gasper@gmail.com>
2016-04-27 11:31:56 +0100

    movie search service added

---
+++ app/assets/javascripts/services/MovieSearchService.js
@@ -0,0 +1,14 @@
+recommendNChill.service('MovieSearchService', ['$http', function($http) {
+  var self = this;
+
+  self.searchFor = function(title) {
+    return $http.get('https://api.themoviedb.org/3/search/movie?query=' + title + '&api_key=40b339f0fde8924dd93e33c92bb397ef')
+    .then(_handleResponseFromApi);
+  };
+
+  function _handleResponseFromApi(response) {
+    return response.data.map(function(searchResults) {
+      return searchResults.title;
+    });
+  };
+}]);
